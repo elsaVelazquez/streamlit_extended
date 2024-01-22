978a2e59c5531a431b59a7e905a085f75b501570
Elsa Velazquez <35537584+elsaVelazquez@users.noreply.github.com>
2024-01-22 16:21:09 -0700

    Create the_app.py

---
+++ the_app.py
@@ -0,0 +1,58 @@
+from openai import AzureOpenAI
+import re
+import streamlit as st
+from salesprompts import get_system_prompt
+
+# Azure OpenAI credentials
+api_key = st.secrets.api_key
+azure_endpoint = st.secrets.api_base
+api_type = st.secrets.api_type
+api_version = st.secrets.api_version
+
+
+st.title("🪒 Edgewell AI")
+
+# Initialize the chat messages history
+client = AzureOpenAI(
+    api_key=api_key, api_version=api_version, azure_endpoint=azure_endpoint
+)
+if "messages" not in st.session_state:
+    # system prompt includes table information, rules, and prompts the LLM to produce
+    # a welcome message to the user.
+    st.session_state.messages = [{"role": "system", "content": get_system_prompt()}]
+
+# Prompt for user input and save
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# display the existing chat messages
+for message in st.session_state.messages:
+    if message["role"] == "system":
+        continue
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        if "results" in message:
+            st.dataframe(message["results"])
+
+# If last message is not from assistant, we need to generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        response = ""
+        resp_container = st.empty()
+        for delta in client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+            stream=True,
+        ):
+            response += (delta.choices[0].delta.content or "")
+            resp_container.markdown(response)
+
+        message = {"role": "assistant", "content": response}
+        # Parse the response for a SQL query and execute if available
+        sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
+        if sql_match:
+            sql = sql_match.group(1)
+            conn = st.connection("snowflake")
+            message["results"] = conn.query(sql)
+            st.dataframe(message["results"])
+        st.session_state.messages.append(message)
